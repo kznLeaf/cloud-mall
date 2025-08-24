c713d542321e2dc063cd61dbb1e02891a105898b
banyee19 <kunpengdu43@gmail.com>
2025-08-24 14:13:12 +0800

    完成hm-api模块的item-service client

--- cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -4,9 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @MapperScan("com.hmall.cart.mapper")
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.hmall.api.client")
 @SpringBootApplication
 public class CartApplication {
     public static void main(String[] args) {
