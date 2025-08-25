da84e325f14539120e282472dc072a3360f7239d
banyee19 <kunpengdu43@gmail.com>
2025-08-25 10:17:59 +0800

    网关part1

---
+++ hm-gate/src/main/java/com/hmall/gateway/GateApplication.java
@@ -0,0 +1,16 @@
+package com.hmall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>Project: hmall</p>
+ * <p>Date: 2025/8/25 9:33</p>
+ * Description:
+ */
+@SpringBootApplication
+public class GateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateApplication.class, args);
+    }
+}
